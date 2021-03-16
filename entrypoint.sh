d1fee2511134e22276b48a2f366be9ab4e261a1c
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-03-16 01:14:05 +0100

    Remove PNG generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 echo "Generating STL from $1..."
 openscad "$1" -o /tmp/output.stl
-echo "Rendering PNG from $1..."
-openscad "$1" --viewall --autocenter -o /tmp/render.png
+#echo "Rendering PNG from $1..."
+#openscad "$1" --viewall --autocenter -o /tmp/render.png
